80dc0b0b1b68e384e046b8b03e5a5b348e2f432c
air <talko.alexander@gmail.com>
2025-03-06 01:29:07 +0400

    upd config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from .tasks import FetchPageTask, ParseXmlTask
 from dotenv import load_dotenv
 import os
 
@@ -6,17 +7,16 @@ import os
 load_dotenv()
 
 # Используем переменные окружения
-broker_url = os.getenv("CELERY_BROKER_URL")  # Без значения по умолчанию
-result_backend = os.getenv("CELERY_RESULT_BACKEND")  # Без значения по умолчанию
-
-# Проверяем, что переменные окружения заданы
-if not broker_url or not result_backend:
-    raise ValueError("Не заданы переменные окружения CELERY_BROKER_URL и CELERY_RESULT_BACKEND")
+broker_url = os.getenv("CELERY_BROKER_URL")
+result_backend = os.getenv("CELERY_RESULT_BACKEND")
 
 app = Celery('tasks', broker=broker_url, backend=result_backend)
 
-# Включаем eager-режим для тестирования
+# Регистрируем задачи
+app.register_task(FetchPageTask())
+app.register_task(ParseXmlTask())
+
 app.conf.update(
-    task_always_eager=True,  # Задачи выполняются синхронно
+    task_always_eager=False,  # Задачи выполняются синхронно
     result_expires=3600,
 )
